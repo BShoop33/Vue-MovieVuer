9c30d57de254dfbcbfc6246f3a9bbff7f6b5330c
Braxton Shoop <braxton.shoop@gmail.com>
2021-03-26 12:30:37 -0500

    adding title selection completed, movie details and reviews completed, project fully completed

--- src/components/MovieReviews.vue
+++ src/components/MovieReviews.vue
@@ -1,20 +1,14 @@
 <template>
   <div v-if="reviews && reviews.length > 0">
     <h3 class="red--text">User Reviews</h3>
-    <v-list three-line>
-      <v-list-item v-for="review in reviews" :key="review.id">
-        <v-list-item-avatar>
-          <v-img
-            :src="`https://image.tmdb.org/t/p/w200${review.author_details.avatar_path}`"
-          ></v-img>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>{{ review.author }}</v-list-item-title>
-          <v-list-item-subtitle> {{ review.created_at }} </v-list-item-subtitle>
-          <div class="mt-2">{{ review.content }}</div>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
+    <ul>
+      <li v-for="review in reviews" :key="review.id">
+        <p>{{ review.author_details.avatar_path }}</p>
+        <p>{{ review.author }}</p>
+        <p>{{ review.created_at }}</p>
+        <p>{{ review.content }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -10,8 +10,16 @@
         Found {{ searchResults.total_results }} results for "{{ searchTerm }}"
       </h3>
       <v-slide-group class="pb-5" show-arrows>
-        <v-slide-item v-for="movie in searchResults.results" :key="movie.id">
+        <!-- <v-slide-item v-for="movie in searchResults.results" :key="movie.id"> -->
+        <v-slide-item v-for="movie in viewableMovies" :key="movie.id">
+          <!-- <v-img
+            contain
+            :src="`https://image.tmdb.org/t/p/w200${movie.poster_path}`"
+            height="200"
+            width="150"
+          ></v-img> -->
           <v-img
+            @click="selectMovie(movie)"
             contain
             :src="`https://image.tmdb.org/t/p/w200${movie.poster_path}`"
             height="200"
@@ -24,12 +32,15 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
+import { mapState, mapGetters, mapActions } from "vuex";
 
 export default {
   computed: {
     ...mapState(["searchResults", "searchTerm"]),
     ...mapGetters(["viewableMovies"]),
+  },
+  methods: {
+    ...mapActions(["selectMovie"]),
   },
 };
 </script>
