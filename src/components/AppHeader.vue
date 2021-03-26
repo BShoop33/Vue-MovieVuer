44f2de4b7894e620993aa79a8f8d464a0832f7b7
Braxton Shoop <braxton.shoop@gmail.com>
2021-03-26 11:13:11 -0500

    search results bug resolved

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,27 +1,29 @@
 <template>
-  <v-app-bar app color="green darken-3" dark>
-    <v-row justify="space-between" align="center">
-      <v-col :sm="6">
-        <div class="d-flex align-center">
-          <h1>Movie Vuer</h1>
-        </div>
-      </v-col>
+  <div>
+    <v-app-bar app color="green darken-3" dark>
+      <v-row justify="space-between" align="center">
+        <v-col :sm="6">
+          <div class="d-flex align-center">
+            <h1>Movie Vuer</h1>
+          </div>
+        </v-col>
 
-      <v-col :sm="2" class="mt-5">
-        <form @submit.prevent="handleSubmit">
-          <v-text-field
-            v-model="query"
-            dense
-            rounded
-            filled
-            placeholder="Search movie titles"
-            background-color="red lighten-2"
-            prepend-inner-icon="mdi-magnify"
-          ></v-text-field>
-        </form>
-      </v-col>
-    </v-row>
-  </v-app-bar>
+        <v-col :sm="2" class="mt-5">
+          <form @submit.prevent="handleSubmit">
+            <v-text-field
+              v-model="query"
+              dense
+              rounded
+              filled
+              placeholder="Search movie titles"
+              background-color="red lighten-2"
+              prepend-inner-icon="mdi-magnify"
+            ></v-text-field>
+          </form>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
@@ -37,8 +39,8 @@ export default {
     ...mapActions(["searchMovies"]),
 
     handleSubmit() {
-      console.log("Search for:", this.query);
       this.searchMovies(this.query);
+      // console.log(this.query);
     },
   },
 };
